56e9ad79886bc0ade06c24f9d6e8837a2bbe99cd
Albert Richard Caputo III <rcaputo3@outlook.com>
2018-09-20 13:17:24 -0400

    Cleaned up divideAndConquer. Added credit to original publisherof find_eulerian_tour.

--- divideAndConquer.py
+++ divideAndConquer.py
@@ -1,28 +1,21 @@
-# Compute a*b in O(log(n)) time where a = n
+# REC_RUSSIAN:  Compute a*b in O(log(n)) time where a = n
+# Input:        integers a, b
+# Output:       integer a*b 
 def rec_russian(a,b):
     if a == 0:
         return 0
     if a % 2 == 0: return 2*rec_russian(a/2,b)
     return b + 2*rec_russian((a-1)/2,b)
 
+# Example and comparison against built-in *
 print(rec_russian(100123,2**24))
 print(100123*(2**24))
 
-def clique(n):
-    print("in a clique...")
-    for j in range(n):
-        for i in range(j):
-            print(i, "is friends with", j)
+# find_eulerian_tour: Finds a Eulerian Tour in a graph (each node traversed exactly once, starting and ending at same node)
+# Original Publisher: Wentao - StackOverflow Jul 15 '16 at 23:56
+# Input:        A graph represented as a list of tuples (edged between nodes)
+# Output:       A list of nodes that represent a Eulerian Tour
 
-print(clique(4))
-
-# Find Eulerian Tour
-#
-# Write a function that takes in a graph
-# represented as a list of tuples
-# and return a list of nodes that
-# you would follow on an Eulerian Tour
-#
 # For example, if the input graph was
 # [(1, 2), (2, 3), (3, 1)]
 # A possible Eulerian tour would be [1, 2, 3, 1]
@@ -46,4 +39,5 @@ def find_eulerian_tour(graph):
             if node in edge:
                 search += [[path, next_node(edge, node), remove_edge(unexplore, edge)]]
 
+# Example:
 print(find_eulerian_tour([(1, 2), (2, 3), (3, 1)]))
